aed03008e7774a1f0196b967e260369e8916ce08
iamprashant <iamprashantjain2601@gmail.com>
2024-09-12 21:41:43 +0530

    performed assesment, cleaning, ed, preprocessing, model training & then created modular code fort data_ingestion.py file

--- README.md
+++ README.md
@@ -13,4 +13,15 @@
 #### 10. src --> create a folder "components" & inside it create a __init__ file, folder contains all the modules like data_ingestion (reading data), data_transformation, model_trainer, model_pusher
 #### 11. src --> create a folder "pipeline" & inside it create a __init__ file & train_pipeline & predict_pipeline
 #### 12. Also create logger, exception & utils.py (to keep common functions) file inside src folder
-#### 
+
+
+#### 13. Until now we have created a structure of a ml project which will remain common for all ML Projects, From now on we will try to convert our ml project jupyter code into modular coding
+#### 14. Add a new folder "notebook" to keep the data file & jupyter file of ml project where we perform EDA, Feature Engineering, Model Building etc
+#### 15. We are assuming that the dataset we have is cleaned by the data scraping team like feature engineering, missing value imputation, duplicates etc so whatever techniques we will use in data transformation & Model building, We will convert those into modular coding except EDA. EDA we are using to understand the techniques to use to pre-process the data
+#### 16. Prepare X, y variables and perform Scaling like standardization or normalization & encoding like OHE or label in a pipeline
+#### 17. All the helper functions which i've used, we will keep those in utils
+#### 18. Perform model training in jupyter to understand all the steps & best methods which we can use in modular coding
+#### 19. When we work in a team, we will have different teams working on webscraping data or preparing/collecting data and saving them into databases or locally or on cloud, Our job as a data scientist is to fetch data from that source & perform cleaning, eda, feature engineering, preprocessing, building models & deploy that on production
+#### 20. In src > components > data_ingestion.py : we will write code to read data fom data source which can be either a database or api. for now, we will use the data file saved locally & split into train & test
+#### 21. We will be reading already cleaned data, we only have to perform encoding & scaling.
+#### 22. Also we need to update gitignore to not read artifacts folder
---
+++ src/components/data_ingestion.py
@@ -0,0 +1,66 @@
+import os
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from src.exception import CustomException
+from src.logger import logging
+
+
+@dataclass
+class DataIngestionConfig:
+    # Paths for storing training, testing, and raw data
+    train_data_path: str = os.path.join('artifacts', 'train_csv')
+    test_data_path: str = os.path.join('artifacts', 'test_csv')
+    raw_data_path: str = os.path.join('artifacts', 'raw_data_csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        """
+        Initialize DataIngestion with the default configuration, as soon as object is created for this class, this will initialize the paths 
+        """
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        """
+        Handles the process of data ingestion: reading data, splitting into
+        training and testing sets, and saving these datasets.
+        """
+        logging.info("Entered into data ingestion method.")
+        
+        try:
+            # Load the dataset from an Excel file
+            # we can also load data from dbs, api, cloud etc
+            df = pd.read_excel(r"H:\CampusX_DS\week43 - My Projects Aug 2024\mlops_laptop_price_prediction\notebook\smartprix_laptop_cleaned_v8.xlsx")
+            logging.info("Read dataset which was already cleaned as DataFrame.")
+            
+            # Ensure the directory for raw data exists
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+            
+            # Save the raw dataset to CSV
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            logging.info("Raw data saved.")
+            
+            # Split the dataset into training and testing sets
+            logging.info("Train-test split initiated.")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            
+            # Save the training and testing datasets to CSV
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            
+            logging.info("Data ingestion completed.")
+            
+            # Return the paths of training and testing data to be used in the next step which is data_transformation
+            return (self.ingestion_config.train_data_path, self.ingestion_config.test_data_path)
+        
+        except Exception as e:
+            # Raise a custom exception if an error occurs
+            raise CustomException(e, sys)
+        
+        
+        
+if __name__ == "__main__":
+    obj = DataIngestion()                   #this will initialize the path of train, test & raw
+    obj.initiate_data_ingestion()           #this will create artifacts folder & update the logs
